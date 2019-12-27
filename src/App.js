2d9f4bd9d55b168933a66c917ad8cf4db724642d
Hyeeun <hyeeuni.mok@gmail.com>
2019-12-26 23:24:34 -0500

    add layout and hoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Testing</h1>
+        <Layout>
+          <p>Testing...</p>
+        </Layout>
       </div>
     );
   }
